6e7edb6363912f5344dae600d96345d87b2611cb
ndonga <ndonga.edwin@gmail.com>
2025-01-27 12:17:12 +0300

    configure resume template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { InterestsComponent } from './components/interests/interests.component';
+import { AwardsComponent } from './components/awards/awards.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'education', component: EducationComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'interests', component: InterestsComponent },
+    { path: 'awards', component: AwardsComponent },
+    { path: '', redirectTo: 'about', pathMatch: 'full'},
+];
